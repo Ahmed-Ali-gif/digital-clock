69899881eaa1832ff250cf0586289d7c18d5ea00
codewithme380@gmail.com <codewithme@gmail.com>
2026-07-12 14:40:38 +0500

    version #2

--- script/script.js
+++ script/script.js
@@ -2,5 +2,30 @@ const timeConatiner = document.querySelector(".timeConatiner");
 let time = new Date();
 timeConatiner.append(time.toLocaleTimeString());
 
-
-
+const list = document.querySelectorAll("li");
+ switch(time.getDay()){
+  case 0:
+    list[0].innerText ="Sunday";
+    break;
+  case 1:
+    list[0].innerText ="Monday";
+  case 2:
+    list[0].innerText ="Tuesday";
+    break;
+  case 3:
+    list[0].innerText ="Wednesday";
+    break;
+  case 4:
+    list[0].innerText ="Thursday";
+    break;
+  case 5:
+    list[0].innerText ="Friday";
+    break;
+  case 6:
+    list[0].innerText ="Saturday";
+    break;
+  default:
+    list[0].innerText ="Inavlid value";
+    break;
+}
+list[1].innerText = time.toLocaleDateString()
