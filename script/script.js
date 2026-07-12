91eb9da1db13d79e569f3a60e84c6ea7631f3541
codewithme380@gmail.com <codewithme@gmail.com>
2026-07-12 15:57:37 +0500

    version #3

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 const timeConatiner = document.querySelector(".timeConatiner");
 let time = new Date();
 timeConatiner.append(time.toLocaleTimeString());
-
 const list = document.querySelectorAll("li");
 switch (time.getDay()) {
   case 0:
@@ -29,16 +28,34 @@ switch (time.getDay()) {
     break;
 }
 list[1].innerText = time.toLocaleDateString();
-console.log(time.getTimezoneOffset());
+console.log(Math.abs(time.getTimezoneOffset() / 60));
 
 //light mode
 const btn = document.querySelector(".btn");
 const changeColor = () => {
-  // document.body.style.backgroundColor = "#F8FAFC";
   const body = document.body;
   let changingBtnText = body.classList.toggle("light-mode");
-  changingBtnText ? btn.innerText ="dark-mode" : btn.innerText= "light-mode"
+  changingBtnText
+    ? (btn.innerText = "dark-mode")
+    : (btn.innerText = "light-mode");
 };
 btn.addEventListener("click", () => {
   changeColor();
+});
+// time zone slection
+const slct = document.querySelector("#select");
+const timeZoneValue = document.querySelector(".time-zone-value");
+const getSelectValue = () => {
+  console.dir(slct.value);
+  let timeZone = Math.abs(slct.value / 60);
+  timeZoneValue.innerText = "+" + timeZone;
+};
+slct.addEventListener("click", () => {
+  getSelectValue();
+});
+const removeSelectValue = () => {
+  timeZoneValue.removeChild(slct.value);
+};
+slct.addEventListener("dbclick", () => {
+  removeSelectValue();
 });
